4f8d3a3d8456d5e6ec26bef4f805504059e1b102
Matteo54 <matteo.conci54@hotmail.it>
2025-05-09 13:38:22 +0200

    add final code to the sql file and updated the readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 - User in Sql can't be used as a table name, so I changed the table name with **_app_user_**;
 - Order in Sql can't be used as a table name, so I changed the table name with **_order_table_**;
 - Sql transform all the word in lower case, unless you put " ", but you have to do it in all the file, so all the table from draw.io will change tha name for example from ProductVariant to product_variant;
-   
+- I changed the product_variant table so now if a product doesn't have a variant at DEFAULT has the value 'default', so we don't need no more to have both product_id and variant_id as foreign key in the other table to compensate if the product doesn't have a variant beacuse now all the product have a default variant;
--- databaseDesign.sql
+++ databaseDesign.sql
@@ -78,7 +78,7 @@ CREATE TABLE homepage_section(
         'categories',
         'brands',
         'wishlist_picks',
-        'reccomended',
+        'recommended',
         'custom'
         )
     ),
@@ -86,7 +86,7 @@ CREATE TABLE homepage_section(
     is_active BOOLEAN NOT NULL DEFAULT true
 );
 
-CREATE TABLE shipping_metod(
+CREATE TABLE shipping_method(
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL UNIQUE,
     description TEXT,
@@ -139,15 +139,15 @@ CREATE TABLE address(
 CREATE TABLE product_variant(
     id SERIAL PRIMARY KEY,
     product_id INTEGER NOT NULL REFERENCES product(id) ON DELETE CASCADE,
-    color TEXT NOT NULL DEFAULT 'defualt',
-    variant TEXT NOT NULL DEFAULT 'defualt',
+    color TEXT NOT NULL DEFAULT 'default',
+    variant TEXT NOT NULL DEFAULT 'default',
     price DECIMAL(10, 2) NOT NULL CHECK (price >= 0),
     stock_quantity INTEGER NOT NULL CHECK (stock_quantity >= 0),
     weight INTEGER CHECK (weight >= 0),
     dimension INTEGER CHECK (dimension >= 0),
-    is_available BOOLEAN NOT NULL,
+    is_available BOOLEAN NOT NULL DEFAULT true,
     sku TEXT NOT NULL UNIQUE,
-    updated_at TIMESTAMP NOT NULL DEFAULT true,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     is_active BOOLEAN NOT NULL DEFAULT true,
     UNIQUE (product_id, color, variant)
 );
@@ -156,24 +156,113 @@ CREATE TABLE order_table(
     id SERIAL PRIMARY KEY,
     app_user_id INTEGER NOT NULL REFERENCES app_user(id),
     address_id INTEGER NOT NULL REFERENCES address(id),
-    status TEXT NOT NULL CHECK (status IN ('pending', 'paid', 'shipped', 'delivered', 'cancelled')),
+    status TEXT NOT NULL CHECK (status IN ('pending', 'paid', 'shipped', 'delivered', 'cancelled', 'returned', 'failed')),
     payment_id INTEGER NOT NULL REFERENCES payment(id),
-    shipping_metod_id INTEGER NOT NULL REFERENCES shipping_metod(id),
+    shipping_method_id INTEGER NOT NULL REFERENCES shipping_method(id),
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE order_item(
-    order_table_id INTEGER NOT NULL REFERENCES order_table(id) ON DELETE CASCADE,
-    variant_id INTEGER REFERENCES product_variant(id),
-    product_id INTEGER REFERENCES product(id),
-    quantity INTEGER NOT NULL DEFAULT 1,
+    order_table_id INTEGER REFERENCES order_table(id) ON DELETE CASCADE,
+    product_variant_id INTEGER REFERENCES product_variant(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL DEFAULT 1 CHECK (quantity > 0),
     price_at_purchase DECIMAL(10, 2) NOT NULL,
+    PRIMARY KEY (order_table_id, product_variant_id)
+);
+
+CREATE TABLE cart(
+    id SERIAL PRIMARY KEY,
+    app_user_id INTEGER NOT NULL UNIQUE REFERENCES app_user(id) ON DELETE CASCADE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP 
+);
+
+CREATE TABLE cart_item(
+    cart_id INTEGER REFERENCES cart(id) ON DELETE CASCADE,
+    product_variant_id INTEGER REFERENCES product_variant(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL CHECK (quantity >= 0),
+    price_at_purchase DECIMAL(10, 2) NOT NULL CHECK (price_at_purchase >= 0),
+    PRIMARY KEY (cart_id, product_variant_id)
+);
+
+CREATE TABLE wishlist(
+    id SERIAL PRIMARY KEY,
+    app_user_id INTEGER NOT NULL REFERENCES app_user(id) ON DELETE CASCADE,
+    name TEXT NOT NULL UNIQUE,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE wishlist_item(
+    wishlist_id INTEGER REFERENCES wishlist(id) ON DELETE CASCADE,
+    product_variant_id INTEGER REFERENCES product_variant(id) ON DELETE CASCADE,
+    added_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (wishlist_id, product_variant_id)
+);
+
+CREATE TABLE homepage_section_item(
+    id SERIAL PRIMARY KEY,
+    section_id INTEGER NOT NULL REFERENCES homepage_section(id) ON DELETE CASCADE,
+    product_variant_id INTEGER REFERENCES product_variant(id),
+    category_id INTEGER REFERENCES category(id),
+    image_url TEXT,
+    link TEXT,
+    title TEXT,
+    subtitle TEXT,
+    position INTEGER NOT NULL CHECK (position >= 0),
     CHECK (
-        (product_id IS NOT NULL AND variant_id IS NULL) OR
-        (product_id IS NULL AND variant_id IS NOT NULL)
+        (product_variant_id IS NOT NULL AND category_id IS NULL)
+        OR (product_variant_id IS NULL AND category_id IS NOT NULL)
+        OR (product_variant_id IS NULL AND category_id IS NULL AND image_url IS NOT NULL)
+    ),
+    UNIQUE (section_id, position)
+);
+
+CREATE UNIQUE INDEX unique_product_per_section ON homepage_section_item(section_id, product_variant_id) WHERE product_variant_id IS NOT NULL;
+CREATE UNIQUE INDEX unique_category_per_section ON homepage_section_item(section_id, category_id) WHERE category_id IS NOT NULL;
+CREATE UNIQUE INDEX unique_image_per_section ON homepage_section_item(section_id, image_url) WHERE image_url IS NOT NULL;
+
+CREATE TABLE product_image(
+    id SERIAL PRIMARY KEY,
+    product_variant_id INTEGER REFERENCES product_variant(id) ON DELETE CASCADE,
+    url TEXT NOT NULL,
+    alt_text TEXT,
+    position INTEGER NOT NULL DEFAULT 0,
+    UNIQUE (product_variant_id, position),
+    UNIQUE (product_variant_id, url)
+);
+
+CREATE TABLE notification(
+    id SERIAL PRIMARY KEY,
+    app_user_id INTEGER NOT NULL REFERENCES app_user(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    content TEXT,
+    is_read BOOLEAN NOT NULL DEFAULT false,
+    type TEXT NOT NULL CHECK (type IN ('order_confirmed', 'order_shipped', 'order_delivered', 'promotion', 'system_alert')),
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE product_discount(
+    discount_id INT REFERENCES discount(id) ON DELETE CASCADE,
+    product_variant_id INT REFERENCES product_variant(id) ON DELETE CASCADE,
+    PRIMARY KEY (discount_id, product_variant_id)
+);
+
+CREATE TABLE review(
+    id SERIAL PRIMARY KEY,
+    app_user_id INTEGER NOT NULL REFERENCES app_user(id),
+    product_id INTEGER REFERENCES product(id) ON DELETE CASCADE,
+    store_id INTEGER REFERENCES store(id) ON DELETE CASCADE,
+    rating INTEGER NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CHECK (    
+        (product_id IS NOT NULL AND store_id IS NULL) OR 
+        (product_id IS NULL AND store_id IS NOT NULL)  
     )
 );
 
-CREATE UNIQUE INDEX unique_product_order ON order_item(order_table_id, product_id) WHERE variant_id IS NULL;
-CREATE UNIQUE INDEX unique_variant_order ON order_item(order_table_id, variant_id) WHERE product_id IS NULL;
+CREATE UNIQUE INDEX unique_product_review_per_user ON review(app_user_id, product_id) WHERE store_id IS NULL;
+CREATE UNIQUE INDEX unique_store_review_per_user ON review(app_user_id, store_id) WHERE product_id IS NULL;
